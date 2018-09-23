181911bf7b5c344024c145a31c158e6a29f7155b
23prime <23.prime.37@gmail.com>
2018-09-24 03:49:20 +0900

    Initial commit

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for oeis
+
+## Unreleased changes
---
+++ src/Math/OEIS/Types.hs
@@ -0,0 +1,50 @@
+module Math.OEIS.Types where
+
+import qualified Data.Text as T
+
+type SeqData = [Integer]
+type Texts = [T.Text]
+
+data SearchStatus = ID String | SubSeq SeqData deriving (Show, Eq)
+
+--data Language = Mathematica | Maple | Other deriving (Show, Eq)
+
+data Keyword = Base | Bref | Changed | Cofr | Cons | Core | Dead | Dumb | Dupe |
+               Easy | Eigen | Fini | Frac | Full | Hard | More | Mult |
+               New | Nice | Nonn | Obsc | Sign | Tabf | Tabl | Uned |
+               Unkn | Walk | Word
+  deriving (Eq, Show, Read)
+
+data OEISData = INT Integer
+              | SEQ SeqData
+              | TXT T.Text
+              | TXTS Texts
+              | KEYS [Keyword]
+--              | PRGS [(Language, T.Text)]
+  deriving (Show)
+
+data OEISSeq
+  = OEIS { number      :: T.Text,
+           ids         :: T.Text,
+           seqData     :: SeqData,
+           name        :: T.Text,
+           comment     :: Texts,
+           reference   :: Texts,
+           link        :: Texts,
+           formula     :: Texts,
+           example     :: Texts,
+           maple       :: Texts,
+           mathematica :: Texts,
+--           program     :: [(Language, T.Text)],
+           program     :: Texts,
+           xref        :: Texts,
+           keyword     :: [Keyword],
+           offset      :: Integer,
+           author      :: T.Text,
+           ext         :: Texts,
+           references  :: Integer,
+           revision    :: Integer,
+           time        :: T.Text,
+           created     :: T.Text
+         }
+  deriving (Show)
