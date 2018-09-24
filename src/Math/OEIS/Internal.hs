5f32695b6f040dbc6a241f24414661198fdf20b8
23prime <a23b23c23d23e@gmail.com>
2018-09-24 22:01:07 +0900

    Make possible to get all search results from sub-sequence

--- src/Math/OEIS/Internal.hs
+++ src/Math/OEIS/Internal.hs
@@ -39,11 +39,11 @@ keys = "keyword" : intKeys ++ textKeys ++ textsKeys :: Texts
 ----------------------
 -- Get JSON of OEIS --
 ----------------------
-baseSearchURI :: String
-baseSearchURI = "http://oeis.org/search?fmt=json&q="
+baseSearchURI :: Int -> String
+baseSearchURI n = "http://oeis.org/search?fmt=json&start=" ++ show n ++"&q="
 
 idSearchURI :: String -> String
-idSearchURI n = baseSearchURI ++ "id:" ++ n
+idSearchURI n = baseSearchURI 0 ++ "id:" ++ n
 
 showSeqData :: SeqData -> String
 showSeqData = tail . init . show
@@ -51,25 +51,44 @@ showSeqData = tail . init . show
 readSeqData :: String -> SeqData
 readSeqData str = read ("[" ++ str ++ "]")
 
-seqSearchURI :: SeqData -> String
-seqSearchURI subSeq = baseSearchURI ++ showSeqData subSeq
+seqSearchURI :: SeqData -> Int -> String
+seqSearchURI subSeq n = baseSearchURI n ++ showSeqData subSeq
 
 openURL :: String -> IO T.Text
 openURL x = fmap T.pack $ getResponseBody =<< simpleHTTP (getRequest x)
 
-getJSON :: SearchStatus -> IO T.Text
-getJSON (ID str)        = openURL $ idSearchURI str
-getJSON (SubSeq subSeq) = openURL $ seqSearchURI subSeq
+getJSON :: SearchStatus -> Int -> IO T.Text
+getJSON (ID str) _        = openURL $ idSearchURI str
+getJSON (SubSeq subSeq) n = openURL $ seqSearchURI subSeq n
 
 
 ----------------
 -- Parse JSON --
 ----------------
+-- Get all search results --
+getResults :: SearchStatus -> Int -> Maybe [Value] -> IO (Maybe [Value])
+getResults ss n vs = do
+  jsn <- getJSON ss n
+  let results' = jsn ^? key "results" . _Array
+      len = length $ fromJust results'
+      results = case results' of
+        Nothing -> return Nothing
+        _       ->
+          let vs' = Just $ (\i -> jsn ^?! key "results" . nth i) <$> [0..(len - 1)]
+          in case ss of
+               ID _ -> return vs'
+               SubSeq _ ->
+                case len of
+                  10 -> getResults ss (n + 10) $ (++) <$> vs <*> vs'
+                  _  -> return $ (++) <$> vs <*> vs'
+  results
+
+
 -- Get search result --
-getResult :: SearchStatus -> IO (Maybe Value)
-getResult ss = do
-  jsn <- getJSON ss
-  let result = jsn ^? key "results" . nth 0
+getResult :: SearchStatus -> Int -> Int -> IO (Maybe Value)
+getResult ss n m = do
+  results <- getResults ss n $ Just []
+  let result = (!! m) <$> results
   return result
 
 -- Get each data in result --
@@ -94,6 +113,7 @@ getData result k
           "keyword" -> (k, KEYS . map readKeyword . T.splitOn "," <$> d)
           "data"    -> let d' = T.unpack $ '[' .+ fromJust d +. ']'
                        in (k, Just $ SEQ (read d' :: SeqData))
+          "id" -> (k, TXTS . T.splitOn " " <$> d)
           _         -> (k, TXT <$> d)
   | k `elem` ("program" : textsKeys)
   = let ds = result ^? key k ._Array
@@ -105,13 +125,12 @@ getData result k
   | otherwise = (k, Nothing)
 
 emptyOEIS :: OEISSeq
-emptyOEIS = OEIS "" "" [] "" [] [] [] [] [] [] [] [] [] [] 0 "" [] 0 0 "" ""
+emptyOEIS = OEIS "" [] [] "" [] [] [] [] [] [] [] [] [] [] 0 "" [] 0 0 "" ""
 
 addElement :: OEISSeq -> (T.Text, Maybe OEISData) -> OEISSeq
 addElement seq (k, Just (TXT t))
   = case k of
       "number"  -> seq {number = t}
-      "id"      -> seq {ids = t}
       "name"    -> seq {name = t}
       "author"  -> seq {author = t}
       "time"    -> seq {time = t}
@@ -119,6 +138,7 @@ addElement seq (k, Just (TXT t))
       _         -> seq
 addElement seq (k, Just (TXTS ts))
   = case k of
+      "id"          -> seq {ids = ts}
       "comment"     -> seq {comment = ts}
       "reference"   -> seq {reference = ts}
       "link"        -> seq {link = ts}
