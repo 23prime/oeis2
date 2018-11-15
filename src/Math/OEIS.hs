519b7efd43f316a9dc214782453376fa3ddbfd8d
23prime <a23b23c23d23e@gmail.com>
2018-11-16 04:39:25 +0900

    Fix comments

--- src/Math/OEIS.hs
+++ src/Math/OEIS.hs
@@ -24,6 +24,7 @@ import           Math.OEIS.Types
 -- | Get all search results on OEIS
 --
 -- e.g.
+--
 -- > ghci>searchSeq (ID "A000027") 0
 -- > [OEIS {number = "A000027", ids = ["M0472","N0173"], seqData = [1,2,3,4,5,6,7,...
 --
@@ -53,6 +54,7 @@ searchSeq ss = unsafePerformIO . searchSeq' ss
 -- | Look up a sequence on OEIS.
 --
 -- e.g.
+--
 -- > ghci>lookupSeq (ID "A000027")
 -- > Just (OEIS {number = "A000027", ids = ["M0472","N0173"], seqData = [1,2,3,4,5,6,7,...
 --
@@ -74,6 +76,7 @@ lookupSeq' ss = do
 -- | Get sub-sequence on OEIS.
 --
 -- e.g.
+--
 -- > ghci>getSeqData (ID "A000027")
 -- > Just [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77]
 --
@@ -91,6 +94,7 @@ getSeqData' = (<$>) (seqData <$>) . lookupSeq'
 -- | Extend from sub-sequence.
 --
 -- e.g.
+--
 -- > ghci>extendSeq [1,2,3,4]
 -- > [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77]
 extendSeq :: SeqData -> SeqData
